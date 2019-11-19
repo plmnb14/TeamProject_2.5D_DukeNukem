b1b606cd365f7a5c4fd1cea331f6847d797c4d66
cho4683 <byeongnam41@gmail.com>
2019-11-19 17:44:10 +0900

    컬리전 몬스터 부분 수정

--- OldMan/Engine/Utilities/Codes/CollisionMgr.cpp
+++ OldMan/Engine/Utilities/Codes/CollisionMgr.cpp
@@ -127,6 +127,40 @@ void CCollisionMgr::CollisionTarget_To_Ground(list<CGameObject*>& rDstList, list
 	}
 }
 
+void CCollisionMgr::CollisionTarget_To_Monstr(list<CGameObject*>& rDstList, list<CGameObject*>& rSrcList)
+{
+	for (auto& rDst : rDstList)
+	{
+		for (auto& rSrc : rSrcList)
+		{
+			ENGINE::CCollider* rDstCol = dynamic_cast<CCollider*>(rDst->Get_Component(L"Collider"));
+			ENGINE::CCollider* rSrcCol = dynamic_cast<CCollider*>(rSrc->Get_Component(L"Collider"));
+
+			ENGINE::CTransform* rDstTrans = dynamic_cast<CTransform*>(rDst->Get_Component(L"Transform"));
+			ENGINE::CTransform* rSrcTrans = dynamic_cast<CTransform*>(rSrc->Get_Component(L"Transform"));
+
+			if (Check_AABB(rDst, rSrc, rDstCol, rSrcCol))
+			{
+				//cout << "충돌체크 됩니다" << endl;
+
+				dynamic_cast<CRigidBody*>(rDst->Get_Component(L"RigidBody"))->Set_IsHit(true);
+				dynamic_cast<CRigidBody*>(rSrc->Get_Component(L"RigidBody"))->Set_IsHit(true);
+				return;
+			}
+			else
+			{
+				//cout << "충돌 안함" << endl;
+				dynamic_cast<CRigidBody*>(rDst->Get_Component(L"RigidBody"))->Set_IsHit(false);
+				dynamic_cast<CRigidBody*>(rSrc->Get_Component(L"RigidBody"))->Set_IsHit(false);
+
+			}
+		}
+
+
+	}
+
+}
+
 bool CCollisionMgr::Check_AABB(ENGINE::CGameObject* rDst , ENGINE::CGameObject* rSrc, CCollider* _rDstCol , CCollider* _rSrcCol)
 {
 	if (_rSrcCol->Get_CollisionType() != ENGINE::COLLISION_AABB)
